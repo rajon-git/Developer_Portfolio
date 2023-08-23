2498d156b42661b1f5c3ac95b018c490c5c92868
rajon-git <rajon.zhsust15@gmail.com>
2023-08-23 22:15:59 +0600

    git calender update

--- src/Component/GitCalender.js
+++ src/Component/GitCalender.js
@@ -1,54 +1,77 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
-import GitHubCalendar  from 'react-github-calendar';
-import explicitTheme from 'react-github-calendar'
+import React, { useState, useEffect } from 'react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import GitHubCalendar from 'react-github-calendar';
+import axios from 'axios';
 
-export default function GitCalender(){
+export default function GitCalendar() {
+  const [contributions, setContributions] = useState([]);
+  const githubUsername = 'rajon-git';
 
-    const selectLastHalfYear = contributions => {
-        const currentYear = new Date().getFullYear();
-        const currentMonth = new Date().getMonth();
-        const shownMonths = 12;
-      
-        return contributions.filter(activity => {
-          const date = new Date(activity.date);
-          const monthOfDay = date.getMonth();
-      
-          return (
-            date.getFullYear() === currentYear &&
-            monthOfDay > currentMonth - shownMonths &&
-            monthOfDay <= currentMonth
-          );
-        });
-      };
+  useEffect(() => {
+    fetchGitHubData();
+  }, []);
 
-    return (
-        <Box bg="#2F2FA1" pt='25px' textAlign='center'>
-            <Heading p='25px' color='#FF9398' size='2xl'>GitHub</Heading>
-            <Box p='40px 20px' w='97%' m='auto' textAlign='center' borderTop='1px solid #FF9398'>
-            <Box w='95%' m='auto' p='45px' className="react-activity-calendar"  color='white'>
-            <GitHubCalendar
-                username="SumatM"
-                blockSize={20}
-                blockMargin={10}
-                fontSize={20}
-                fontColor='white'
-                hideTotalCount='true'
-               
-            />
-            </Box>
-            <Flex  mt='40px' justify='space-around'>
-                <Box >
-                    <img id="github-stats-card" src='https://github-readme-stats.vercel.app/api?username=SumatM'/>
-                </Box>
-                <Box >
-            <img id="github-streak-stats"  src="https://github-readme-streak-stats.herokuapp.com?user=SumatM&theme=neon-blurange"/>
-            </Box>
-            </Flex>
-            
-            <Box  w='30%' m='auto' mt='40px'>
-              <img width='150%' id="github-top-langs" src="https://github-readme-stats.vercel.app/api/top-langs/?username=SumatM"/>
-                </Box>
-            </Box>
+  const fetchGitHubData = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${githubUsername}/events`);
+      console.log('Event Data:', response.data); // Log the event data
+      const eventData = response.data.filter(event => event.type === 'PushEvent');
+      console.log('Filtered Contributions:', eventData); // Log the contributions after filtering
+      setContributions(selectLastHalfYear(eventData));
+    } catch (error) {
+      console.error('Error fetching GitHub data:', error);
+    }
+  };
+
+  const selectLastHalfYear = contributions => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 12;
+
+    return contributions.filter(activity => {
+      const date = new Date(activity.date);
+      const monthOfDay = date.getMonth();
+
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
+
+  return (
+    <Box bg="#2F2FA1" pt='25px' textAlign='center'>
+      <Heading p='25px' color='#FF9398' size='2xl'>GitHub</Heading>
+      <Box p='40px 20px' w='97%' m='auto' textAlign='center' borderTop='1px solid #FF9398'>
+        <Box w='95%' m='auto' p='45px' className="react-activity-calendar" color='white'>
+          <GitHubCalendar
+            username="rajon-git"
+            blockSize={20}
+            blockMargin={10}
+            fontSize={20}
+            fontColor='white'
+            hideTotalCount='true'
+
+          />
         </Box>
-    )
+        <Flex mt='40px' justify='space-around'>
+          <Box>
+            <img id="github-stats-card" src={`https://github-readme-stats.vercel.app/api?username=${githubUsername}`} alt="GitHub Stats" />
+          </Box>
+          <Box>
+            <img id="github-streak-stats" src={`https://github-readme-streak-stats.herokuapp.com?user=${githubUsername}&theme=neon-blurange`} alt="GitHub Streak Stats" />
+          </Box>
+        </Flex>
+        <Box w='30%' m='auto' mt='40px'>
+          <img
+            width='150%'
+            id="github-top-langs"
+            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${githubUsername}`}
+            alt="GitHub Top Languages"
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 }
